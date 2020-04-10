2d3c250d3acabb95aeb06d15a5164e0449b57a1a
youssefRagab <youssefmousa2001@gmail.com>
2020-04-10 22:11:09 +0200

    added checkmarks

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import Todos from './todos';
 class App extends Component {
   state = {
     "todos":[
-      {id:"1", title:"buy milk"},
-      {id:"2", title:"buy car"},
+      {id:"1", title:"buy milk", isChecked: false},
+      {id:"2", title:"buy car", isChecked: false},
     ]
   }
   deleteTodo = (id) => {
     var newList = this.state.todos.filter((item) => {
-      return item.id !== id ? item: null;
+      return item.id !== id;
     });
     this.setState({
       todos: newList
     })
   }
+  setCheckMark = (id) => {
+    var List = this.state.todos;
+    for(var i = 0; i < List.length; i++) {
+      var obj = List[i];
+      if(obj.id == id) {
+        if(obj.isChecked) {
+          obj.isChecked = false;
+        }else {
+          obj.isChecked = true
+        }
+      }
+    }
+
+    this.setState({todos:List});
+  }
   render() {
     return (
       <div className="todo-app container">
         <h2 class="center-align blue-text">Todo's</h2>
-        <Todos todos={this.state.todos} deleteItem={this.deleteTodo} />
+        <Todos todos={this.state.todos} deleteItem={this.deleteTodo} setCheckMark={this.setCheckMark} />
       </div>
     );
   }
--- src/todos.js
+++ src/todos.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-function todos({todos, deleteItem}) {
+function todos({todos, deleteItem, setCheckMark}) {
     const todosList = todos.length ? (
         todos.map((item) => {
+            var checked = false;
+            if(item.isChecked) {
+                checked = "checked";
+            }
             return(
                 <div className="collection-item" key={item.id}>
                     <span onClick={() => {deleteItem(item.id)}}>{item.title}</span>
+                    <label>
+                        <input onClick={() => {setCheckMark(item.id)}} type="checkbox" class="filled-in" checked={checked} />
+                        <span></span>
+                    </label>                    
                 </div>
             );
         })
     ) : (
-        <p className="center">You have no todos, yay!</p>
+        <p className="center">You have no todo's left, yay!</p>
     ); 
     return(
         <div className="todos collection">
